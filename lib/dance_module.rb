0d32f8b6f548dd8d7005dca1ceb79c09f806c885
SamHarron <harronsam@gmail.com>
2020-12-07 23:31:33 -0500

    commit tree a4593477aa9ebe966bf504a211e4a341626f730a

---
+++ lib/dance_module.rb
@@ -0,0 +1,23 @@
+
+#This is a module called Dance, which 
+#contains methods that we intended 
+#to be used as //instances methods\\ in 
+#the //Dancer\\ class (dancer.rb)
+
+module Dance
+    def twirl
+        "I'm twirling!"
+      end
+    
+      def jump
+        "Look how high I'm jumping!"
+      end
+    
+      def pirouette
+        "I'm doing a pirouette"
+      end
+    
+      def take_a_bow
+        "Thank you, thank you. It was a pleasure to dance for you all."
+      end
+end
